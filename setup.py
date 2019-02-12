39294657789be98e7338958a5c31b98468af51a5
LanderU <lander.usategui@gmail.com>
2019-02-12 11:07:19 +0100

    Added long_description

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ import setuptools
 
 setuptools.setup(
     name="hros",
-    version="0.0.1",
+    version="0.04",
     author="Acutronic Robotics",
     author_email="contact@acutronicrobitcs.com",
     description="H-ROS command line utility",
+    long_description='''
+    This tool will help you to communicate and configure the H-ROS SoM. Each H-ROS SoM will integrate an API interface to customize some critical aspects, such as security or ros_domain_id.
+    ''',
     url="https://github.com/acutronicrobitcs/h-ros-cli",
     entry_points = {
       'console_scripts': ['hros=hros.__main__:main'],
